095dc12e5cfb94c8994e499d61aaa68a2f9de778
Neyverson <neyversonjunior@gmail.com>
2025-05-03 19:37:55 -0300

    Alterando a identidade visual do site, colocando a página dos cursos

--- Home/urls.py
+++ Home/urls.py
@@ -11,6 +11,14 @@ urlpatterns = [
     path("", views.index, name="index"),
     # Rota para a página de Matemática
     path('matematica/', views.matematica, name='matematica'),
+    path('matematica/6matematica/', views.matematica6, name='6matematica'),
+    path('matematica/7matematica/', views.matematica7, name='7matematica'),
+    path('matematica/8matematica/', views.matematica8, name='8matematica'),
+    path('matematica/9matematica/', views.matematica9, name='9matematica'),
     # Rota para a página de Português
     path('portugues/', views.portugues, name='portugues'),
+    path('portugues/6portugues', views.portugues6, name='6portugues'),
+    path('portugues/7portugues', views.portugues7, name='7portugues'),
+    path('portugues/8portugues', views.portugues8, name='8portugues'),
+    path('portugues/9portugues', views.portugues9, name='9portugues'),
 ]
--- Home/views.py
+++ Home/views.py
@@ -12,5 +12,37 @@ def matematica(request):
     return render(request, 'Home/matematica.html')
 
 
+def matematica6(request):
+    return render(request, 'Home/matematica/6matematica.html')
+
+
+def matematica7(request):
+    return render(request, 'Home/matematica/7matematica.html')
+
+
+def matematica8(request):
+    return render(request, 'Home/matematica/8matematica.html')
+
+
+def matematica9(request):
+    return render(request, 'Home/matematica/9matematica.html')
+
+
 def portugues(request):
     return render(request, 'Home/portugues.html')
+
+
+def portugues6(request):
+    return render(request, 'Home/portugues/6portugues.html')
+
+
+def portugues7(request):
+    return render(request, 'Home/portugues/7portugues.html')
+
+
+def portugues8(request):
+    return render(request, 'Home/portugues/8portugues.html')
+
+
+def portugues9(request):
+    return render(request, 'Home/portugues/9portugues.html')
